7164ed5c410044353f44fa411cb4a81a16086377
oykue <oykuergun07@gmail.com>
2022-01-17 16:38:18 +0300

    add para yatir sayfasi

---
+++ parayatir.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="tr">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device.width, initial-scale=1.0, shrink-to-fit=no">
+        <link rel="stylesheet" href="style.css">
+        <title>TürksatBank</title>
+    </head>
+    <body>
+        <header class="MainHeader" >
+            
+        </header>
+    
+        <div class="hesabim_content">
+        </br>
+                <div class="label">
+                    <Label><h3>Hesap Bakiyeniz: </h3></Label>
+                    <Textbox></Textbox>
+                </div></br>
+                <div class="label">
+                    <Label><h3>Yatırmak İstediğiniz Tutarı Giriniz</h3></Label><br>
+                    <input type="text">
+                </div></br>
+                <button type="submit" class="button tamam">Onayla</button>
+                </br>
+                </br>
+                </br>
+               <a href="secimekrani.php"><button class="button tamam">Geri</button></a>
+               <a href="index.php"><button class="button tamam">Çıkış</button>
+        </div>
+    </body>
+</html>
