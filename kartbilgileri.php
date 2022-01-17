45e68abe75a2a82df292f25cd6a630e74ce11d0e
oykue <oykuergun07@gmail.com>
2022-01-17 16:39:53 +0300

    add kart bilgileri sayfasi

---
+++ kartbilgileri.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="tr">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device.width, initial-scale=1.0, shrink-to-fit=no">
+        <link rel="stylesheet" href="style.css">
+        <title>TürksatBank</title>
+    </head>
+    <body>
+        <header class="MainHeader" >
+            
+        </header>
+    
+        <div class="hesabim_content">
+                <div class="label">
+                    <Label><h3>Ad</h3></Label>
+                    <input type="text">
+                </div></br>
+                <div class="label">
+                    <Label><h3>Soyad</h3></Label>
+                    <input type="text">
+                </div></br>
+                <div class="label">
+                    <Label><h3>Kart Numarası</h3></Label>
+                    <input type="text">
+                </div></br>
+                <div class="label">
+                    <Label><h3>Kart Türü</h3></Label>
+                    <input type="text">
+                </div></br>
+                </br>
+               <a href="secimekrani.php"><button class="button tamam">Geri</button></a>
+               <a href="index.php"><button class="button tamam">Çıkış</button>
+        </div>
+    </body>
+</html>
