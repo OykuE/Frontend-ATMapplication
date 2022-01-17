72a5af5bba0ec2455c52c58588f5ce9b8dcff95c
oykue <oykuergun07@gmail.com>
2022-01-17 12:25:16 +0300

    işlem seçim ekranı eklendi.

---
+++ secimekrani.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="tr">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device.width, initial-scale=1.0, shrink-to-fit=no">
+        <link rel="stylesheet" href="style.css">
+        <title>TürksatBank</title>
+    </head>
+    <body>
+        <header class="MainHeader" >   
+        </header>
+        <div class="giris">
+            <h3>Yapmak İstediğiniz İşlem Türünü Seçiniz</h3><br><br>
+                <div class="btn-group">
+                <a href="hesapbilgileri.php"><button class="button tamam">Hesap Bilgilerim</button></a></br>
+                <a href="kartbilgileri.php"><button class="button tamam">Kart Bilgilerim</button></a></br>
+                <a href="paracek.php"><button class="button tamam">Hesaptan Para Çek</button></a></br>
+                <a href="parayatir.php"><button class="button tamam">Hesaba Para Yatır</button></a></br>
+                <a href="paragonder.php"><button class="button tamam">Para Gönder</button></a></br>
+            </div>
+                <br>
+                <a href="index.php"><button type="submit" class="button tamam">İptal</button></a>
+        </div>
+    </body>
+</html>
+
