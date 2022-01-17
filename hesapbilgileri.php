b1bad98cbe1d68f3ddc19d2864ecdedc518b3929
oykue <oykuergun07@gmail.com>
2022-01-17 15:59:43 +0300

    add hesap bilgileri sayfası.

---
+++ hesapbilgileri.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="tr">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device.width, initial-scale=1.0, shrink-to-fit=no">
+        <link rel="stylesheet" href="style.css">
+        <title>TürksatBank</title>
+    </head>
+    <body>
+        <header class="MainHeader" >
+            
+        </header>
+    
+        <div class="hesabim_content">
+        </br>
+        <h1>Hesap Bilgileri</h1></br>
+                <div class="label">
+                    <Label>Ad</Label><br>
+                    <input type="text">
+                </div></br>
+                <div class="label">
+                    <Label>Soyad</Label><br>
+                    <input type="text">
+                </div></br>
+                <div class="label">
+                    <Label>Hesap Numarası</Label><br>
+                    <input type="text">
+                </div></br>
+                <div class="label">
+                    <Label>Hesap Bakiyesi</Label><br>
+                    <input type="text">
+                </div></br>
+                </br>
+               <a href="secimekrani.php"><button class="button tamam">Geri</button></a>
+               <a href="index.php"><button class="button tamam">Çıkış</button>
+        </div>
+    </body>
+</html>
