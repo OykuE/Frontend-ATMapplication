0c04a356e23d9488284b5ba6aff4eed4f3ff4d55
oykue <oykuergun07@gmail.com>
2022-01-19 17:14:38 +0300

    update main page design

--- index.php
+++ index.php
@@ -3,7 +3,8 @@
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device.width, initial-scale=1.0 shrink-to-fit=no">
-        <link rel="stylesheet" href="css/style.css"> 
+        <link href="css\bootstrap.min.css" rel="stylesheet"/>
+        <link href="css\style.css" rel="stylesheet"/>
         <title>TürksatBank</title>
     </head>
     <body>
@@ -12,9 +13,10 @@
         </header>
     
         <div class="giris">
-        </br>
-        <h1>Hoş Geldiniz</h1></br>
-            <h3>Lütfen Giriş Yapınız</h3>
+        </br></br>
+         <h1>Hoş Geldiniz</h1></br>
+         </br></br>
+         <h3>Lütfen Giriş Yapınız</h3></br>
             <form action="islem.php" method="post">
                 <div class="user">
                     <input type="text" name="tc" placeholder="TC Kimlik No">
@@ -23,10 +25,13 @@
                     <input type="password" name="password" placeholder="Şifre">
                 </div>
                 </br>
-                <button type="submit" class="button tamam">Tamam</button>
-                
             </form>
            
+        </div>
+        <div class="footer">
+           <div div class="btn-group" role="group">
+                 <button type="submit" class="btn btn-onay">Tamam</button>
+             </div>
         </div>
     </body>
 </html>
