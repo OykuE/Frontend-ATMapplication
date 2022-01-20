69236d8149535a293007db3378f2c13cf7e06fd1
oykue <oykuergun07@gmail.com>
2022-01-20 12:04:38 +0300

    add hesapsecim page

---
+++ hesapsecim.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="tr">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device.width, initial-scale=1.0, shrink-to-fit=no">
+        <link href="css\bootstrap.min.css" rel="stylesheet"/>
+        <link href="css\style.css" rel="stylesheet"/>
+        <title>TürksatBank</title>
+    </head>
+    <header class="MainHeader"> </header>
+    <body>
+        <div class="giris">
+            <h3>Yapmak İstediğiniz İşlem Türünü Seçiniz</h3><br><br> 
+            <div class="list-group">
+                 <a href="hesapbilgileri.php" class="list-group-item list-group-item-secim">Vadesiz Hesabım</a>
+                 <a href="secimekrani.php" class="list-group-item list-group-item-secim">Vadeli Hesabım</a>
+                 <a href="secimekrani.php" class="list-group-item list-group-item-secim">Yatırım Hesabım</a>
+                 <a href="secimekrani.php" class="list-group-item list-group-item-secim">Döviz Hesabım</a>
+            </div>
+        </div>
+        <div class="footer">
+                    <div class="btn-group" role="group">
+                        <a href="secimekrani.php"><button type="button" class="btn btn-iptal">İptal</button></a>
+                    </div>
+                    <div class="btn-group" role="group">
+                        <button type="submit" class="btn btn-onay">Tamam</button>
+        </div>
+
+   
+    </body>
+</html>
