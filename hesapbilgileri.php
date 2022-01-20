f34ea8d9cf733360656904ca132bc4c568cc358e
oykue <oykuergun07@gmail.com>
2022-01-20 12:05:43 +0300

    update hesapbilgileri page to hesapsecim page

--- hesapbilgileri.php
+++ hesapbilgileri.php
@@ -3,7 +3,8 @@
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device.width, initial-scale=1.0, shrink-to-fit=no">
-        <link rel="stylesheet" href="css\bootstrap.min.css">
+        <link href="css\bootstrap.min.css" rel="stylesheet"/>
+        <link href="css\style.css" rel="stylesheet"/>
         <title>TürksatBank</title>
     </head>
     <body>
@@ -11,27 +12,30 @@
             
         </header>
     
-        <div class="hesabim_content">
-                <div class="label">
-                    <Label><h3>Ad</h3></Label>
-                    <input type="text">
-                </div></br>
-                <div class="label">
-                    <Label><h3>Soyad</h3></Label>
-                    <input type="text">
-                </div></br>
-                <div class="label">
-                    <Label><h3>Hesap Numarası</h3></Label>
-                    <input type="text">
-                </div></br>
-                <div class="label">
-                    <Label><h3>Hesap Bakiyesi</h3></Label>
-                    <input type="text">
-                </div></br>
-                </br>
-               <a href="secimekrani.php"><button class="button tamam">Geri</button></a>
-               <a href="index.php"><button class="button tamam">Çıkış</button>
-               
+    <div class="giris">
+                <h3 class="panel-title">Vadesiz TL</h3>
+                <div class="panel-body">
+                     <Label><h4>Ad</h4></Label><br>
+                     <label class="output"><b>Öykü</b></label><br><br>
+              
+                     <Label><h4>Soyad</h4></Label><br>
+                     <label class="output"><b>Ergün</b></label><br><br>
+                
+                     <Label><h4>Hesap Numarası</h4></Label><br>
+                    <label class="output"><b>12345678912</b></label><br><br>
+
+                     <Label><h4>Hesap Bakiyesi</h4></Label><br>
+                     <label class="output"><b>100.0 TL</b></label><br><br>    
+                </div> 
+     </div>     
+         <div class="footer">
+                    <div class="btn-group" role="group">
+                        <a href="secimekrani.php"><button type="button" class="btn btn-iptal">İptal</button></a>
+                    </div>
+                    <div class="btn-group" role="group">
+                        <button type="submit" class="btn btn-onay">Tamam</button>
         </div>
+
+
     </body>
 </html>
