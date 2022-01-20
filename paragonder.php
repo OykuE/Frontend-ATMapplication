4b06688e649ffb5a196b66e4d782c03e63530132
oykue <oykuergun07@gmail.com>
2022-01-20 13:47:26 +0300

    update paragonder page style update tamam button update iptal button

--- paragonder.php
+++ paragonder.php
@@ -3,7 +3,8 @@
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device.width, initial-scale=1.0, shrink-to-fit=no">
-        <link rel="stylesheet" href="style.css">
+        <link href="css\bootstrap.min.css" rel="stylesheet"/>
+        <link href="css\style.css" rel="stylesheet"/>
         <title>TürksatBank</title>
     </head>
     <body>
@@ -11,26 +12,31 @@
             
         </header>
     
-        <div class="hesabim_content">
+        <div class="giris">
         </br>
+        </br> 
                 <div class="label">
-                    <Label><h3>Hesap Bakiyeniz: </h3></Label>
-                    <Textbox></Textbox>
-                </div></br>
+                    <Label><h3>Hesap Bakiyeniz:</h3></Label></br>
+                    <label class="output"><b>0.0 TL</b></label><br><br>
+                </div></br></br></br></br></br>
                 <div class="label">
-                    <Label><h3>Para Gönderilecek Hesabın IBAN Numarasını Giriniz</h3></Label><br>
-                    <input type="text">
-                </div></br>
-                <div class="label">
-                    <Label><h3>Göndermek İstediğiniz Tutarı Giriniz</h3></Label><br>
-                    <input type="text">
-                </div></br>
-                <button type="submit" class="button tamam">Onayla</button>
-                </br>
-                </br>
-                </br>
-               <a href="secimekrani.php"><button class="button tamam">Geri</button></a>
-               <a href="index.php"><button class="button tamam">Çıkış</button>
+                    <Label><h3>Para Gönderilecek Hesabın IBAN Numarasını Giriniz</h3></Label>
+                    <div class="user">
+                    <input class="output" type="text" name="iban" placeholder="0.0">
+                    </div>
+                    <Label><h3>Gönderilecek Tutarı Giriniz</h3></Label>
+                    <div class="user">
+                    <input class="output" type="text" name="gonderilecektutar" placeholder="0.0">
+                    </div>
+                </div>
         </div>
+                <div class="footer">
+                    <div class="btn-group" role="group">
+                        <a href="secimekrani.php"><button type="button" class="btn btn-iptal">İptal</button></a>
+                    </div>
+                    <div class="btn-group" role="group">
+                        <button type="submit" class="btn btn-onay">Tamam</button>
+                </div>
+        
     </body>
 </html>
