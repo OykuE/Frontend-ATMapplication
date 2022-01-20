ef520b77812ace805483f12bab886cabad56340e
oykue <oykuergun07@gmail.com>
2022-01-20 13:44:01 +0300

    add borcode page

---
+++ borcode.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="tr">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device.width, initial-scale=1.0, shrink-to-fit=no">
+        <link href="css\bootstrap.min.css" rel="stylesheet"/>
+        <link href="css\style.css" rel="stylesheet"/>
+        <title>TürksatBank</title>
+    </head>
+    <body>
+        <header class="MainHeader" >
+            
+        </header>
+    
+        <div class="giris">
+        </br>
+        </br> 
+                <div class="label">
+                    <Label><h3>Hesap Bakiyeniz:</h3></Label></br>
+                    <label class="output"><b>0.0 TL</b></label><br>
+                    <Label><h3>Dönem Borcu:</h3></Label></br>
+                    <label class="output"><b>0.0 TL</b></label>
+                    <Label><h3>Asgari Ödenecek Tutar:</h3></Label></br>
+                    <label class="output"><b>0.0 TL</b></label><br><br>
+                </div></br></br></br></br></br></br></br></br></br></br></br></br>
+                    <Label><h3>Ödemek İstediğiniz Tutarı Giriniz</h3></Label>
+                    <div class="user">
+                    <input class="output" type="text" name="gonderilecektutar" placeholder="0.0">
+                    </div>
+                </div>
+        </div>
+                <div class="footer">
+                    <div class="btn-group" role="group">
+                        <a href="secimekrani.php"><button type="button" class="btn btn-iptal">İptal</button></a>
+                    </div>
+                    <div class="btn-group" role="group">
+                        <button type="submit" class="btn btn-onay">Tamam</button>
+                </div>
+        
+    </body>
+</html>
